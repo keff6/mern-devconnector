5945bbe0ec9780be9ab93c9ece2453b08139e136
keff6 <kev.fallas@gmail.com>
2018-04-28 12:12:30 -0600

    Add JWT authentication

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://kevin.fallas:123@ds119489.mlab.com:19489/devconnector"
+  mongoURI: "mongodb://kevin.fallas:123@ds119489.mlab.com:19489/devconnector",
+  secretOrKey: "secret"
 };

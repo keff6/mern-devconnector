8d312c267e4bb1de08607548a63d1cd5cd2d42f4
keff6 <kev.fallas@gmail.com>
2018-07-17 19:54:27 -0600

    Add errorReducer and register flow

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combinedReducers, combineReducers } from 'redux';
 import authReducer from './authReducer';
+import errorReducer from './errorReducer';
+
 
 export default combineReducers({
   auth: authReducer,
+  errors: errorReducer,
 });

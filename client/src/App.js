c362f24061170689b869c13740d6bbe8fefa8f7e
keff6 <kev.fallas@gmail.com>
2018-07-27 15:04:01 -0600

    Add Create Profile form component

--- client/src/App.js
+++ client/src/App.js
@@ -13,8 +13,10 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/create-profile/CreateProfile';
 
 import './App.css';
+
 
 // Check for token
 if (localStorage.jwtToken) {
@@ -49,6 +51,7 @@ class App extends Component {
               <Route exact path="/login" component={Login} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
+                <PrivateRoute exact path="/create-profile" component={CreateProfile} />
               </Switch>
             </div>
             <Footer />
